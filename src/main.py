497f1d4c9e47bfbbe1568025865d986e1dc87845
Emanuel Gomes <101026578+kaqui-null@users.noreply.github.com>
2023-07-21 21:36:00 -0300

    Bugfix na inicialização de objeto player e inicialização do loop principal

--- src/main.py
+++ src/main.py
@@ -1,15 +1,45 @@
 import curses
+from curses import wrapper
+
+
 from player import Player
 
+# coloquem o tamanho do mapa aqui ou mudem a variável na inicialização tbm
+Y = 0
+X = 0
+WINDOW_SIZE = (Y, X)
 
-def main():
-    player = player.Player()
+# A origem do player
+DIRECTION = "up"
+MAP_ORIGIN = (Y, X, DIRECTION)
 
+def main(stdsrc):
+    stdsrc.nodelay(True)
+
+    try:
+        curses.curs_set(0)
+    except:
+        pass
+
+    
+    player = Player(WINDOW_SIZE, MAP_ORIGIN)
+
+    game_running = True
+    while game_running:
+        try:
+            key = stdsrc.getch()
+        except:
+            key = None
+
+        if key == 27:
+            break
+        else:
+            continue
 
 
 # inicialização na função main em main.py
 if __name__ == "__main__":
-    main()
+   wrapper(main) 
 
 
 
--- src/player.py
+++ src/player.py
@@ -3,8 +3,22 @@ import curses
 
 class Player:
     
-    def __init__(self):
+    def __init__(self, window_size, origin):
+        self.window_size = window_size
+        self.window_y = window_size[0]
+        self.window_x = window_size[1]
+        self.origin = origin
+        self.origin_y = origin[0]
+        self.origin_x = origin[1]
+        self.direction = origin[2]
+
+    def draw_sprite(self):
         pass
+
+    def walk(self):
+        pass
+
+
 
 
 
