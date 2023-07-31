8fb5cf8b34682991cf6990d7073456e80a718df9
Emanuel Gomes <101026578+kaqui-null@users.noreply.github.com>
2023-07-31 18:07:36 -0300

    bug fix no dano do player

--- src/main.py
+++ src/main.py
@@ -27,31 +27,29 @@ def main(stdsrc):
     enemy = Enemy(stdsrc, 3, 1, WINDOW_SIZE[0] + 3, WINDOW_SIZE[1] + 1, DIRECTION, [player.origin_y, player.origin_x])
 
     game_running = True
-    turn = 0
-    prev_turn = turn
+    key = 0
     while game_running:
+        player.draw_sprite()
+        enemy.draw_sprite()
+        
         try:
             key = stdsrc.getkey()
         except:
             key = None
 
-        if key == "q":
-            break
-        else:
-            prev_turn = turn
-            turn += 1
+        if key != None:
+            if key == "q":
+                break
         
-        stdsrc.erase()
-        player.walk(key)
-        player.draw_sprite()
-        enemy.draw_sprite()
+            stdsrc.erase()
+            player.walk(key)
+            player.draw_sprite()
+            enemy.draw_sprite()
 
-        enemy.player_pos = [player.origin_y, player.origin_x]
-        if enemy.attack() == True:
-            player.is_hurt(enemy.forca)
-        else:
-            continue
-
+            enemy.player_pos = [player.origin_y, player.origin_x]
+            if enemy.attack() == True:
+                player.is_hurt(enemy.forca)
+        
         stdsrc.refresh()
     
     # debug --
