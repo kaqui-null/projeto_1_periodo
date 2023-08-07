b3b59801c2a68313107eb49b6bed62a6fe6f7597
benny <124681388+alocinny@users.noreply.github.com>
2023-08-07 16:14:22 -0300

    sala-teste

---
+++ src/mapa/mapa.py
@@ -0,0 +1,15 @@
+import curses
+from curses import textpad
+from curses import wrapper
+
+def mapa(stdscr):
+    
+    curses.curs_set(False)
+    y, x = stdscr.getmaxyx()
+    
+    textpad.rectangle(stdscr, 5, 30, y-5, x-30)
+
+    stdscr.getch()
+    stdscr.refresh()
+    
+wrapper(mapa)
