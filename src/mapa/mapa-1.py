eba88c40f5fb92f27bad2a7b6a061ed29139d914
benny <124681388+alocinny@users.noreply.github.com>
2023-08-15 17:33:40 -0300

    esboço do mapa 1

--- src/mapa/mapa-1.py
+++ src/mapa/mapa-1.py
@@ -2,34 +2,33 @@ import curses
 from curses import wrapper
 from curses import textpad
 
-
-def criando_sala(stdscr, y, x, height, width):
-    textpad.rectangle(stdscr, y, x, y + height, x + width)
-    return (y, x, height, width)
+def criando_sala(stdscr, y_sala, x_sala, height, width):
+    textpad.rectangle(stdscr, y_sala, x_sala, y_sala + height, x_sala + width)
+    return (y_sala, x_sala, height, width)
 
 def mapa_1(stdscr):
+
+    stdscr = stdscr
     curses.curs_set(0)
     stdscr.clear()
+    stdscr.refresh()
     
-    #salas | ainda vou alterar os tamanhos das salas
-    salas = [
-        criando_sala(stdscr, 2, 5, 5, 10),
-        criando_sala(stdscr, 8, 20, 6, 8),
-        criando_sala(stdscr, 15, 5, 8, 12),
-        criando_sala(stdscr, 2, 40, 4, 10),
-        criando_sala(stdscr, 12, 35, 5, 15),
-        criando_sala(stdscr, 5, 65, 7, 10),
-        criando_sala(stdscr, 15, 60, 6, 12),
+    #salas | tamanhos ajustados
+    salas = [ 
+        criando_sala(stdscr, 4, 5, 8, 16),
+        criando_sala(stdscr, 23, 35, 10, 13),
+        criando_sala(stdscr, 20, 5, 10, 15),
+        criando_sala(stdscr, 4, 65, 6, 30),
+        criando_sala(stdscr, 14, 110, 15, 15),
+        criando_sala(stdscr, 13, 30, 6, 22),
+        criando_sala(stdscr, 21, 70, 8, 15),
     ]
 
-    for i in range(len(salas) - 1):
-        y1, x1, _, _ = salas[i]
-        y2, x2, _, _ = salas[i + 1]
-    
-    stdscr.refresh()
+    corredor = [
+        criando_corredor(stdscr, 20,20)
+    ]
+
     stdscr.getch()
 
 wrapper(mapa_1)
-
-
 
