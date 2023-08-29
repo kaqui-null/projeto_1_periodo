00de8ad4a40dbdb2f4170b19ecd4bd8401083be1
benny <124681388+alocinny@users.noreply.github.com>
2023-08-29 18:36:54 -0300

    mensagem de game over

---
+++ src/pop_up/gameover.py
@@ -0,0 +1,37 @@
+import curses
+from curses import wrapper
+import time
+
+def game_over(stdscr):
+    stdscr.clear()
+    curses.curs_set(0)
+    curses.resize_term(35,135)
+
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+    RED_AND_BLACK = curses.color_pair(1)
+
+    gameOver = "GAME OVER!!"
+    y,x = stdscr.getmaxyx()
+
+    y= y//2
+    x = x//2-len(gameOver)
+
+    while 1:
+
+        stdscr.refresh()
+
+        stdscr.addstr(y,x, gameOver)
+
+        stdscr.refresh()
+        curses.napms(1000)
+
+        stdscr.attron(RED_AND_BLACK)
+        stdscr.addstr(y,x, gameOver)
+        stdscr.attroff(RED_AND_BLACK)
+
+        stdscr.refresh()
+        curses.napms(1000)
+
+    stdscr.getch()
+wrapper(game_over)
+
